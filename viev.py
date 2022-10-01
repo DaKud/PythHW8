f211d28aa5b55d864078e7580adc639d8d516a2f
DaKu <do.holyada@gmail.com>
2022-10-01 16:56:31 +0300

    1

--- viev.py
+++ viev.py
@@ -2,15 +2,15 @@
 
 def get_data():
     
-    return f"{input('Введите фамилию: ')} {input('Введите имя: ')} {input('Введите класс: ')}\n"
+    return f"{input('Input Surname: ')} {input('Input Name: ')} {input('Input Group No: ')}\n"
 
 def get_number_operasion():
-    return input('Выберите № нужной категории\n \
-        где: 1-показать все фамилии\n \
-             2-показать все имена\n \
-             3-показать все № классов\n \
-             4-показать учеников нужного вам класса\n \
-             5-показать всю таблицу\n \
-             6-показать информацию по ученику\n \
-             7-добавить ученика\n \
-             8-выход\n ')    
+    return input('Pick No \n \
+        where: 1-show all Surnames\n \
+             2-show all Names\n \
+             3-show all Group No\n \
+             4-show the students of definite Group\n \
+             5-show all the list\n \
+             6-show the whole student information \n \
+             7-add a student\n \
+             8-exit\n ')    
